ab3c4f78b75e4b2a188404b6de37ebbbf76df73b
Erwan Boisard <72559018+erwaan@users.noreply.github.com>
2024-04-29 14:22:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import click
 
-# Fonction pour calculer le prix total à payer par personne
+# Calcul du prix total pour une personne
 def calculer_prix_total_par_personne(prix_total, nombre_personnes, nuits_par_personne, nuits_totales):
     prix_par_nuit = prix_total / nuits_totales
     prix_total_personne = [nuits * prix_par_nuit for nuits in nuits_par_personne]
     return prix_total_personne
 
+# Commande CLICK
 @click.command()
 def main():
-    # Demander le nombre de personnes
-    nombre_personnes = click.prompt("Entrez le nombre de personnes", type=int)
-
-    prix_total = 1964  # Prix total de la villa
-
-    # Initialiser une liste pour stocker le nombre de nuits pour chaque personne
     nuits_par_personne = []
     nuits_totales = 0
+    
+    # Nombre de personnes total à la villa des bg
+    nombre_personnes = click.prompt("Entrez le nombre de personnes", type=int)
 
-    # Boucle pour demander le prénom et le nombre de nuits pour chaque personne
+    # Prix total de la villa des bg en euro
+    prix_total = 1964  
+
+    # Boucle prompt prénom et nombre nuit
     for i in range(nombre_personnes):
         prenom = click.prompt(f"Entrez le prénom de la personne {i+1}")
         nuits = click.prompt(f"Combien de nuits {prenom} reste-t-il ?", type=int)
         nuits_par_personne.append(nuits)
         nuits_totales += nuits
 
-    # Calculer le prix total à payer par personne
     prix_total_personne = calculer_prix_total_par_personne(prix_total, nombre_personnes, nuits_par_personne, nuits_totales)
 
-    # Afficher le prix total à payer par personne
+    # Affichage du résultat de la moulaga à rendre
     click.echo("Prix total à payer par personne:")
     for prenom, prix, nuits in zip(range(1, nombre_personnes + 1), prix_total_personne, nuits_par_personne):
         click.echo(f"Personne {prenom}: {prix:.2f} € pour {nuits} nuits")
